4fd57c1ecde862d106eb5047e9306f95d71a1466
ashishpal07 <ashishpal7861@gmail.com>
2024-08-19 13:28:43 +0530

    css changes

--- app/components/OverallScoreCard.jsx
+++ app/components/OverallScoreCard.jsx
@@ -8,7 +8,7 @@ export const OverallScoreCard = () => {
     <div className='flex items-center justify-between bg-white rounded-lg p-3 shadow-md'>
       <div>
         <p className='text-gray-500 text-sm'>Overall Score</p>
-        <h2 className='text-sm md:text-2xl lg:text-2xl font-semibold'>
+        <h2 className='text-md sm:text-2xl lg:text-2xl font-semibold'>
           Remark: <span className='text-green-600'>Good</span>
         </h2>
         <p className='text-gray-400 text-xs'>Evaluates on {currentDate}</p>
